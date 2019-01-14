ca84185018f5efb94fb6ad7229aaea517a9c0d6f
Fer Hernandez <fh.1111@gmail.com>
2019-01-14 01:16:20 +0000

    Automatically backed up by Learn

--- db/migrate/20190114011358_create_appointments.rb
+++ db/migrate/20190114011358_create_appointments.rb
@@ -2,10 +2,9 @@ class CreateAppointments < ActiveRecord::Migration[5.0]
   def change
     create_table :appointments do |t|
       t.datetime :appointment_datetime
-      t.integer :doctor_id
-      t.integer :patient_id
-
-      t.timestamps
+	    t.belongs_to :doctor, index: true, foreign_key: true
+	    t.belongs_to :patient, index: true, foreign_key: true
+	    t.timestamps null: false
     end
   end
 end
